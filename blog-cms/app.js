8e14e5c9b12b2bdfdaaacbd9b342e90da57900e6
Summer Basehart <summer@Judes-Mac-mini.local>
2020-01-16 14:21:33 -0800

    tst app models

--- blog-cms/app.js
+++ blog-cms/app.js
@@ -3,8 +3,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
+var passport = require('passport');
 
-
+var auth = require('./routes/auth');
+var category = require('./routes/category');
+var post = require('./routes/post');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -18,13 +21,19 @@ mongoose.connect('mongodb://localhost/blog-cms', {
 
 var app = express();
 
+app.use(passport.initialize());
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/auth', auth);
+app.use('/api/category', category);
+app.use('/api/post', post);
 
 module.exports = app;
