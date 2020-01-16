b36e098d2c11ef265ec1916901187c1c1514a559
Summer Basehart <summer@Judes-Mac-mini.local>
2020-01-16 14:11:57 -0800

    scratch

---
+++ blog-cms/app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+mongoose.connect('mongodb://localhost/blog-cms', {
+    promiseLibrary: require('bluebird'),
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(() =>  console.log('connection successful'))
+  .catch((err) => console.error(err));
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
