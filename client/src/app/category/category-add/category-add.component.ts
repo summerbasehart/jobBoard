3ea7b64a990b1b438645feba1add5ba1d12eba1a
Summer Basehart <summer@Judes-Mac-mini.local>
2020-01-16 15:40:30 -0800

    category-add updated ts and html

--- client/src/app/category/category-add/category-add.component.ts
+++ client/src/app/category/category-add/category-add.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CategoryService } from '../../category.service';
+import { FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-category-add',
@@ -7,9 +19,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryAddComponent implements OnInit {
 
-  constructor() { }
+  categoryForm: FormGroup;
+  catName = '';
+  catDesc = '';
+  catImgUrl = '';
+  catContent = '';
+  isLoadingResults = false;
+  matcher = new MyErrorStateMatcher();
+
+  constructor(private router: Router, private api: CategoryService, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.categoryForm = this.formBuilder.group({
+      catName : [null, Validators.required],
+      catDesc : [null, Validators.required],
+      catImgUrl : [null, Validators.required],
+      catContent : [null, Validators.required]
+    });
+  }
+
+  onFormSubmit() {
+    this.isLoadingResults = true;
+    this.api.addCategory(this.categoryForm.value)
+      .subscribe((res: any) => {
+          const id = res._id;
+          this.isLoadingResults = false;
+          this.router.navigate(['/category-details', id]);
+        }, (err: any) => {
+          console.log(err);
+          this.isLoadingResults = false;
+        });
   }
 
 }
