bab4564dfedaefba2a3e02a67a7f18b2db0decf8
Summer Basehart <summer@Judes-Mac-mini.local>
2020-01-16 15:27:48 -0800

    login/register ts and html

--- client/src/app/category/category.component.ts
+++ client/src/app/category/category.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryService } from '../category.service';
+import { Category } from './category';
 
 @Component({
   selector: 'app-category',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['catName', 'catDesc'];
+  data: Category[] = [];
+  isLoadingResults = true;
+
+  constructor(private api: CategoryService) { }
 
   ngOnInit() {
+    this.api.getCategories()
+      .subscribe((res: any) => {
+        this.data = res;
+        console.log(this.data);
+        this.isLoadingResults = false;
+      }, err => {
+        console.log(err);
+        this.isLoadingResults = false;
+      });
   }
 
 }
