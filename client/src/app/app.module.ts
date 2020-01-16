9d1b1d5f1aba3fb312a03a835d2fc7b050f608d7
Summer Basehart <summer@Judes-Mac-mini.local>
2020-01-16 14:44:27 -0800

    install client side

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+import { BycategoryComponent } from './bycategory/bycategory.component';
+import { DetailsComponent } from './details/details.component';
+import { CategoryComponent } from './category/category.component';
+import { CategoryDetailsComponent } from './category/category-details/category-details.component';
+import { CategoryAddComponent } from './category/category-add/category-add.component';
+import { CategoryEditComponent } from './category/category-edit/category-edit.component';
+import { PostComponent } from './post/post.component';
+import { PostDetailsComponent } from './post/post-details/post-details.component';
+import { PostAddComponent } from './post/post-add/post-add.component';
+import { PostEditComponent } from './post/post-edit/post-edit.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    AdminComponent,
+    BycategoryComponent,
+    DetailsComponent,
+    CategoryComponent,
+    CategoryDetailsComponent,
+    CategoryAddComponent,
+    CategoryEditComponent,
+    PostComponent,
+    PostDetailsComponent,
+    PostAddComponent,
+    PostEditComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ client/src/app/bycategory/bycategory.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bycategory',
+  templateUrl: './bycategory.component.html',
+  styleUrls: ['./bycategory.component.scss']
+})
+export class BycategoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
