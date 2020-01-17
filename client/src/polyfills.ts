bb4cee8dd9cc32e384a9960bbb020b3627823599
Summer Basehart <summer@Judes-Mac-mini.local>
2020-01-17 14:06:56 -0800

    changing polyfills

--- client/src/polyfills.ts
+++ client/src/polyfills.ts
@@ -61,3 +61,4 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+import 'web-animations-js';  // Run `npm install --save web-animations-js`.
