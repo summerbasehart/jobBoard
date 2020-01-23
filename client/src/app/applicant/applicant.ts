a98a85f20d0f9f7d924190a3fa3ae64c2c633769
Summer Basehart <summer@Judes-Mac-mini.local>
2020-01-23 14:32:08 -0800

    applicant schema

---
+++ client/src/app/applicant/applicant.ts
@@ -0,0 +1,11 @@
+export class Applicant {
+    category: string;
+    id: string;
+    appName: string;
+    appPhone: string;
+    appEmail: string;
+    appAddress1: string;
+    appAddress2: string;
+    appResume: string;
+    appSubmit: Date
+  }
