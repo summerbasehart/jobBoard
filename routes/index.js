e03f9624599be991a54b9f4c9b763dce40cfd7cc
Summer Basehart <summer@Judes-Mac-mini.local>
2020-01-27 11:49:02 -0800

    remove extra functions

--- routes/index.js
+++ routes/index.js
@@ -39,23 +39,4 @@ router.get('/applicant', function(req, res, next) {
   });
 });
 
-// router.get('/applicant/:id', function(req, res, next) {
-//   Applicant.findById(req.params.id, function (err, applicant) {
-//     if (err) return next(err);
-//     res.json(applicant);
-//   });
-// });
-
-// router.post('/join-ost', function(req, res, next){
-//   var emailInfo = req.body;
-//   if(!emailInfo.name || !emailInfo.email || !emailInfo.phone){
-//       res.status(400).send({success: false});
-//   } else {
-//       mailer.contactEmail2(emailInfo).then(function(info){
-//           console.log(info);
-//           res.status(200).send({success:true});
-//       });
-//   }
-// });
-
 module.exports = router;
