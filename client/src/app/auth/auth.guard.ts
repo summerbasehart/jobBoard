72aa0a4b366da797fe43e6af97dcbba36daa5ba6
Summer Basehart <summer@Judes-Mac-mini.local>
2020-01-29 09:23:43 -0800

    trying to pull postTitle into applicants

--- client/src/app/auth/auth.guard.ts
+++ client/src/app/auth/auth.guard.ts
@@ -24,7 +24,7 @@ export class AuthGuard implements CanActivate {
     this.authService.redirectUrl = url;
 
     // Navigate to the login page with extras
-    this.router.navigate(['/home']);
+    this.router.navigate(['/login']);
     return false;
   }
 
